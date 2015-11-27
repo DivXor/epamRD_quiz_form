83569a65d0e6713773df16ea34946f3866ad1037
Ilya Doroshenko <divon.step@gmail.com>
2015-11-27 13:34:45 +0600

    change class name

---
+++ src/main/java/kz/epam/quiz/model/service/impl/MailoutMessageServiceImpl.java
@@ -0,0 +1,9 @@
+package kz.epam.quiz.model.service.impl;
+
+import kz.epam.quiz.model.entity.History;
+import kz.epam.quiz.model.service.HistoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailoutMessageServiceImpl extends GenericServiceImpl<Integer, History> implements HistoryService {
+}
