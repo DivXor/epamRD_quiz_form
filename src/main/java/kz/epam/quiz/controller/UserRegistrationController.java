1bcf8af6a4ae8ef7b98205ec53819f359862dc23
admin <mr_spec@mail.ru>
2015-11-27 14:31:40 +0600

    khamid 4.0

--- src/main/java/kz/epam/quiz/controller/UserRegistrationController.java
+++ src/main/java/kz/epam/quiz/controller/UserRegistrationController.java
@@ -3,6 +3,10 @@ package kz.epam.quiz.controller;
 import kz.epam.quiz.model.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by admin on 11/27/2015.
@@ -12,9 +16,9 @@ public class UserRegistrationController {
     @Autowired
     private UserService userService;
 
-
-    public String doRegistration() {
-
-        return "";
+    @RequestMapping(path = "/registration", method = RequestMethod.POST)
+    public String doRegistration(HttpServletRequest request) {
+        String referer = request.getHeader("Referer");
+        return "redirect:" + referer;
     }
 }
