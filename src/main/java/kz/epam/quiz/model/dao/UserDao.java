f72983093b9cdfab018f017617aa3cd82bf9e31f
Ilya Doroshenko <divon.step@gmail.com>
2015-11-28 20:48:53 +0600

    changed to spring data jpa

--- src/main/java/kz/epam/quiz/model/dao/UserDao.java
+++ src/main/java/kz/epam/quiz/model/dao/UserDao.java
@@ -2,11 +2,13 @@ package kz.epam.quiz.model.dao;
 
 import kz.epam.quiz.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface UserDao extends JpaRepository<User, Integer> {
+    @Query()
     List<User> getUsersByAnswerId(int id);
 }
